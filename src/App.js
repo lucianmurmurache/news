39c4cd7c9ffe0dc001291616c23ecb40bcf263f9
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-01-08 18:36:54 +0100

    Updated gitignore, App file still in the works for basic config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = ()  => {
+
+  const alanKey = ''
+  /* **** To read ****
+  
+  https://alan.app/docs/usage/getting-started
+  */
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey
+    })
+  }, [])
+
   return (
     <div>
       <h1>News app</h1>
