f222a06bf5035757799a197344432c589fb2f090
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-01-13 19:27:10 +0100

    Updated favicon and added new components
    Error on Alan backend, does not recognise articles, wroking on fix
    Components not finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const App = ()  => {
+import NewsCards from './components/NewsCards/NewsCards';
 
-  const alanKey = ''
-  /* **** To read ****
-  
-  https://alan.app/docs/usage/getting-started
-  */
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  const alanKey =
+    '548386efaabe2be229176ece4abfcdc22e956eca572e1d8b807a3e2338fdd0dc/stage';
 
   useEffect(() => {
     alanBtn({
-      key: alanKey
-    })
-  }, [])
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
 
   return (
     <div>
       <h1>News app</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
-}
+};
 
 export default App;
