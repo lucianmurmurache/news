db3125eefed58307c6415cd3f9e4eb392c354b52
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-01-14 17:20:38 +0100

    Added Modal component and finished cards

---
+++ src/components/NewsCards/NewsCard/styles.js
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  media: {
+    height: 250,
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderBottom: '10px solid white',
+    boxShadow: '0px 5px 11px -7px #171717, 5px 5px 15px 5px rgba(0,0,0,0)',
+  },
+  activeCard: {
+    borderBottom: '10px solid #171717',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
